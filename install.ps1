46383884fb1728d1209927fbb7ee8e721f6a725b
Anthony Cangialosi <5457010+acangialosi@users.noreply.github.com>
2022-04-17 19:29:26 -0700

    Full VS install.

--- install.ps1
+++ install.ps1
@@ -1,3 +1,6 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+#Commands part of Chocolatey's recommended install steps. Skipping for now.
+#Set-ExecutionPolicy Bypass -Scope Process -Force
+#[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install visualstudio2022enterprise --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
